c0a07bd262d4d89bdf19690892f1d02b370acc2e
revantark <revantark@gmail.com>
2025-02-14 14:33:36 +0530

    update rbf_test to test all

--- btc/rbf_test.go
+++ btc/rbf_test.go
@@ -145,7 +145,7 @@ var _ = Describe("BatchWallet:RBF", Ordered, func() {
 		Expect(err).To(BeNil())
 	})
 
-	FIt("should be able to send funds in smaller amounts", func() {
+	It("should be able to send funds in smaller amounts", func() {
 		for i := 0; i < 10; i++ {
 			req := []btc.SendRequest{
 				{
