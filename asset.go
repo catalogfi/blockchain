26e335132846a13ee9aa1aaac773feaae47280f8
tok-kkk <yunshi@catalog.fi>
2025-08-18 12:01:39 +1000

    add usdc on bnb

--- asset.go
+++ asset.go
@@ -320,11 +320,18 @@ var (
 var (
 	// Mainnet
 
-	AssetBnbSmartChainBnb = Asset{
+	AssetBnbBnbSmartChain = Asset{
 		Chain:    NewEvmChain(BnbSmartChain),
 		Address:  Native,
 		Decimals: 19,
 		Symbol:   "BNB",
+	}
+
+	AssetUsdcBnbSmartChain = Asset{
+		Chain:    NewEvmChain(BnbSmartChain),
+		Address:  "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
+		Decimals: 6,
+		Symbol:   "USDC",
 	}
 )
 
