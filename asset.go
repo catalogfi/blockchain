8ad32a56fe83cefbc508d7893b01ecb4707ee6c0
tok-kkk <yunshi@catalog.fi>
2025-08-18 16:46:17 +1000

    fix usdc decimals on bnb

--- asset.go
+++ asset.go
@@ -330,7 +330,7 @@ var (
 	AssetUsdcBnbSmartChain = Asset{
 		Chain:    NewEvmChain(BnbSmartChain),
 		Address:  "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
-		Decimals: 6,
+		Decimals: 18,
 		Symbol:   "USDC",
 	}
 )
